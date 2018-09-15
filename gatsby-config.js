170fea102658adc1ac870bb1af6f733aae172ca8
carriecarter <carrie@carrie-carter.com>
2018-09-15 12:11:26 -0700

    config and adding plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,26 @@
+require('dotenv').config();
+const SPACE_ID = process.env.SPACE_ID;
+const DELIVERY_API_KEY = process.env.DELIVERY_API_KEY;
+
 module.exports = {
+    siteMetadata: {
+        title: `Carrie Carter Codes`,
+        author: `@pdxcarrie`
+    },
     plugins: [
+        {
+            resolve: `gatsby-source-contentful`,
+            options: {
+                spaceId: SPACE_ID,
+                accessToken: DELIVERY_API_KEY,
+            },
+        },
         {
             resolve: `gatsby-plugin-typography`,
             options: {
                 pathToConfigModule: `src/utils/typography.js`,
             },
         },
-    ],
+        `gatsby-plugin-sass`
+    ]
 }
